ddd9d9e4457df39e8b2b51b58e4fdeef795e595f
Ryan Wallace <ryanwallace024@gmail.com>
2020-10-19 22:31:19 -0400

    diceeee

---
+++ DiceRoller/app/src/main/java/com/cubis/diceroller/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.cubis.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // create new dice object w/ 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        // update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}

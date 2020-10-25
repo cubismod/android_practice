1bc026a81a7790a14d4f16c221e30258f4839835
Ryan Wallace <ryanwallace024@gmail.com>
2020-10-24 21:16:18 -0400

    add images to dice roller app completed

--- DiceRoller/app/src/main/java/com/cubis/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/cubis/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.cubis.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -16,6 +16,8 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+
+        rollDice()
     }
 
     /**
@@ -25,9 +27,20 @@ class MainActivity : AppCompatActivity() {
         // create new dice object w/ 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        // update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        // determine drawable resource ID to use based on dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // update image resource
+        diceImage.setImageResource(drawableResource)
+        // update content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
